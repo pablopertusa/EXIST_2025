5660abe896f9d2f006f7f4323ffb4fd603967e91
pablopertusa <pablopertusa27@gmail.com>
2025-04-30 14:10:39 +0200

    feat: add soft predictions

--- create_submision_folder.py
+++ create_submision_folder.py
@@ -1,6 +1,29 @@
 import json
 import polars as pl
 import os
+import re
+import json
+
+def formatear_string_para_json(json_string):
+    """
+    Formatear una cadena para que sea un JSON válido, corrigiendo
+    principalmente el uso de comillas simples alrededor de las claves.
+
+    Args:
+        json_string (str): La cadena que se intentará formatear.
+
+    Returns:
+        str: La cadena formateada (si se pudo corregir) o la cadena original
+             si no se encontraron problemas evidentes de comillas simples.
+    """
+    formateado = re.sub("'", '"', json_string)
+
+    try:
+        json.loads(formateado)
+        return formateado
+    except json.JSONDecodeError:
+        print("Advertencia: No se pudo formatear completamente la cadena a JSON válido.")
+        return json_string
 
 def create_submision_file(team_name: str, task: int, evaluation_context: str, run_id: str, subtask: int, predictions_folder: str, 
                           submision_folder: str) -> bool:
@@ -9,18 +32,29 @@ def create_submision_file(team_name: str, task: int, evaluation_context: str, ru
     """
     try:
         if exists_submision_folder(team_name):
-            predictions_english_path = predictions_folder + "/" + f"sexism_predictions_task{subtask}_english.csv"
+            predictions_english_path = predictions_folder + "/" + f"sexism_predictions_task{subtask}_{evaluation_context}_english.csv"
             predictions_english = pl.read_csv(predictions_english_path)
-            predictions_spanish_path = predictions_folder + "/" + f"sexism_predictions_task{subtask}_spanish.csv"
+            predictions_spanish_path = predictions_folder + "/" + f"sexism_predictions_task{subtask}_{evaluation_context}_spanish.csv"
             predictions_spanish = pl.read_csv(predictions_spanish_path)
             predictions = pl.concat([predictions_spanish, predictions_english], how="vertical").sort(by="id")
-            output = []
-            for row in predictions.iter_rows(named=True):
-                aux = {}
-                aux["test_case"] = "EXIST2025"
-                aux["id"] = str(row["id"])
-                aux["value"] = row["label"]
-                output.append(aux)
+            if evaluation_context == "hard":
+                output = []
+                for row in predictions.iter_rows(named=True):
+                    aux = {}
+                    aux["test_case"] = "EXIST2025"
+                    aux["id"] = str(row["id"])
+                    aux["value"] = row["label"]
+                    output.append(aux)
+
+            elif evaluation_context == "soft":
+                output = []
+                for row in predictions.iter_rows(named=True):
+                    aux = {}
+                    aux["test_case"] = "EXIST2025"
+                    aux["id"] = str(row["id"])
+                    aux["value"] = json.loads(formatear_string_para_json(row["value"]))
+                    output.append(aux)
+
             filename = submision_folder + "/" + f"task{task}_{subtask}_{evaluation_context}_{team_name}_{run_id}.json"
             with open(filename, "w") as file:
                 json.dump(output, file, indent=4)
